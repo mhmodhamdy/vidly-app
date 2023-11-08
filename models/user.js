0e02a8786259cc07c90e65bbbbf803dff83e8651
Mahmoud Faheem <67551490+mhmodhamdy@users.noreply.github.com>
2023-11-08 14:06:59 +0200

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,47 @@
+
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3,
+    maxlength: 255
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255
+  },
+  isAdmin: Boolean
+});
+
+userSchema.methods.generateAuthToken = function (){
+  const token = jwt.sign({ _id:this._id, isAdmin:this.isAdmin }, config.get('jwtPrivateKey'));
+  return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user){
+  const schema = {
+    name: Joi.string().min(3).max(255).required(),
+    email: Joi.string().min(3).max(255).required().email(),
+    password: Joi.string().min(3).required()
+  }
+  return Joi.validate(user, schema)
+};
+
+exports.User = User;
+exports.validate = validateUser;
